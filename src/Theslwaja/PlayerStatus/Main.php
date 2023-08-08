79a045bf48ab99e856770131323a2f1fc04547a2
Theslowaja <67841659+Theslowaja@users.noreply.github.com>
2023-08-08 23:12:11 +0700

    Change Method #1

--- src/Theslwaja/PlayerStatus/Main.php
+++ src/Theslwaja/PlayerStatus/Main.php
@@ -15,7 +15,7 @@ class Main extends PluginBase implements Listener{
 
     public function onEnable(): void {
 	if($this->getServer()->getPluginManager()->getPlugin("ScoreHud") != null){
-           $this->getServer()->getPluginManager()->registerEvent(new TagResolceEvent(), $this);
+           $this->getServer()->getPluginManager()->registerEvents(new ScoreHudEvent(), $this);
 	}
     }
     
@@ -39,25 +39,5 @@ class Main extends PluginBase implements Listener{
         return true;
     }
 
-    public function onTagResolve(TagsResolveEvent $event){
-	    $player = $event->getPlayer();
-	    $tag = $event->getTag();
-
-        $os = ["Unknown", "Android", "iOS", "macOS", "FireOS", "GearVR", "HoloLens", "Windows 10", "Windows", "Dedicated", "Orbis", "Playstation 4", "Nintento Switch", "Xbox One"];
-        $controls = ["Unknown", "Mouse & Keyboard", "Touch", "Controller"];
-
-	    switch($tag->getName()){
-		    case "playerstatus.device":
-		    	$tag->setValue($os[$player->getPlayerInfo()->getExtraData()["DeviceOS"] ?? 0]);
-		    break;
-
-		    case "playerstatus.control":
-			    $tag->setValue($controls[$player->getPlayerInfo()->getExtraData()["CurrentInputMode"] ?? 0]);
-		    break;
-
-		    case "playerstatus.devicemodel":
-		    	$tag->setValue($player->getPlayerInfo()->getExtraData()["DeviceModel"]);
-		    break;
-	    }
-    }
+    
 }
